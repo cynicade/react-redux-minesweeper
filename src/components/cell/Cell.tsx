67fea2aa3f9fe24ccedc71e7d58f8dbaec4b31bb
cynicade <cynkvs@gmail.com>
2022-06-13 09:04:42 +0200

    fix: styles weren't showing correctly in chrome

--- src/components/cell/Cell.tsx
+++ src/components/cell/Cell.tsx
@@ -23,13 +23,20 @@ export const Cell: React.FC<PropsType> = ({
 }: PropsType): JSX.Element => {
   const dispatch = useAppDispatch();
   const cells = useAppSelector(selectCells);
+  const getBackgroundColor = (): string => {
+    if (cells && cells[y][x].open) return "#fff";
+    return "inherit";
+  };
   const cellStyles = css({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     height: "100%",
     width: "100%",
-    border: "1px solid #BBB",
+    border: "1px solid #999",
+    fontWeight: "bold",
+    fontSize: "18px",
+    backgroundColor: getBackgroundColor(),
   });
 
   if (cells && cells[y][x].open)
@@ -51,8 +58,10 @@ export const Cell: React.FC<PropsType> = ({
         css={{
           width: "100%",
           height: "100%",
-          backgroundColor: "eeeeee",
+          backgroundColor: "#eeeeee",
           border: "none",
+          transitionDuration: "500ms",
+          "&:hover": { backgroundColor: "#c7c5c7" },
         }}
         onClick={() => dispatch(gameActions.openCell({ y, x }))}
         onContextMenu={(e) => {
